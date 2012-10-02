05d98542bf495f50b7613eb9c520489bed2a4e2b
Zoe Wood <zwood1992@gmail.com>
2012-10-02 23:27:17 +0100

    Template Update 1

--- TemplateHandler.php
+++ TemplateHandler.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 	/**
 	 * Template Handler
 	 *
@@ -61,3 +62,41 @@
 // ==========================================================
 // Don't put a closing PHP tag on files that don't need them!
 // ==========================================================
+=======
+    /*Template Handler
+     *
+     *Handles the templates
+     *
+     *@Category Metaldyne
+     *@Package  Metaldyne Template
+     *@Version  1.0
+     *@since    1.0
+     *
+     */
+
+    /*Sets up Twig*/
+    require_once 'Handlers/Libs/Template/Twig/Autoloader.php';
+    Twig_Autoloader::register();
+
+    $loader = new Twig_Loader_Filesystem('./Pages/Templates');
+    $twig = new Twig_Enviroment($loader, array(
+            'cache' => './Cache/Compiled',      /*Cached templates will be stored in said file*/
+            'auto_reload' => TRUE               /*Twig reloads templates everytime changes are made*/
+        ));
+
+    /*Load Template*/
+    $template = $twig->loadTemplate('./Pages/Templates/TemplateA.html');
+
+    /*Retrieves Data from model*/
+    /*array of data (just to check if this shit works*/
+    $datajson = '[{"FirstName": "Zoe", "LastName":Wood"},
+                 {"FirstName":"Elliot", "LastName":"Wright"}
+                 ]';
+    $jsonData = json_decode($datajson, true);
+
+    $template->display(array(
+        'pageTitle' => 'metaldyne.co.uk',
+        'Names' => $jsonData
+    ));
+    ?>
+>>>>>>> Template Update 1
